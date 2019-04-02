db0a86a3ef84a18084274d543f8e2457ac4b8782
Gang <gangl@chalmers.se>
2019-04-02 19:56:12 +0200

    feat: add script and corresponding models from RAVEN2

---
+++ ComplementaryScripts/DraftModels/Source/buildModelFromKEGG_Metacyc.m
@@ -0,0 +1,30 @@
+%% BUild the draft model with RAVEN2 tool box.
+% Try different blast settings to generate several models.
+% 
+% Gang Li
+% 2018-10-28
+clc
+clear
+
+%% 1. get model from KEGG
+dataDir = 'prok90_kegg87';
+fastaFile = '../Dataset/protein_seqeunces.fasta';
+scores = [-50,-40,-30,-20,-10,-1];
+for i = 1:length(scores)
+    score = 10^scores(i);
+    keggmodel = getKEGGModelForOrganism('halo',fastaFile,dataDir,'',1,0,0,0,score);
+    outname = strcat('../Results/halo_keggmodel_',num2str(-scores(i)),'.mat');
+    save(outname,'keggmodel');
+end
+%% 2. get model from Metacyc
+scores = [100,80,60,50];
+for i = 1:length(scores)
+    metacycmodel=getMetaCycModelForOrganism('halo',fastaFile,1,0,0,scores(i));
+    outname = strcat('../Results/halo_metacycmodel_',num2str(scores(i)),'.mat')
+    save(outname,'metacycmodel');
+end
+
+%% 3. save excel format model
+clear
+load ../Results/halo_metacycmodel_100.mat
+exportToExcelFormat(metacycmodel,'../Results/halo_metacycmodel_100.xlsx');
