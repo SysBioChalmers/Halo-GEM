f79fac3055355d929f511e595865bffe3bf3b264
Gang <gangl@chalmers.se>
2020-02-27 16:26:38 +0100

    keep only one model for kegg and metacyc, resp

--- ComplementaryScripts/DraftModels/Source/buildModelFromKEGG_Metacyc.m
+++ ComplementaryScripts/DraftModels/Source/buildModelFromKEGG_Metacyc.m
@@ -8,8 +8,8 @@ clear
 
 %% 1. get model from KEGG
 dataDir = 'prok90_kegg87';
-fastaFile = '../Dataset/protein_seqeunces.fasta';
-scores = [-50,-40,-30,-20,-10,-1];
+fastaFile = '../../../ComplementaryData/protein_sequences_v2.0.fasta';
+scores = [-50];
 for i = 1:length(scores)
     score = 10^scores(i);
     keggmodel = getKEGGModelForOrganism('halo',fastaFile,dataDir,'',1,0,0,0,score);
@@ -17,7 +17,7 @@ for i = 1:length(scores)
     save(outname,'keggmodel');
 end
 %% 2. get model from Metacyc
-scores = [100,80,60,50];
+scores = [100];
 for i = 1:length(scores)
     metacycmodel=getMetaCycModelForOrganism('halo',fastaFile,1,0,0,scores(i));
     outname = strcat('../Results/halo_metacycmodel_',num2str(scores(i)),'.mat')
@@ -25,6 +25,6 @@ for i = 1:length(scores)
 end
 
 %% 3. save excel format model
-clear
-load ../Results/halo_metacycmodel_100.mat
-exportToExcelFormat(metacycmodel,'../Results/halo_metacycmodel_100.xlsx');
+% clear
+% load ../Results/halo_metacycmodel_100.mat
+% exportToExcelFormat(metacycmodel,'../Results/halo_metacycmodel_100.xlsx');
