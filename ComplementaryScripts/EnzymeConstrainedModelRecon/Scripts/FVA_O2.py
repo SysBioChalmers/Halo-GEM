a3f1e9550302bf849874654f99e8493e851f572d
Hao Luo <lhao@chalmers.se>
2020-12-28 13:55:40 +0100

    chore: FVA under different O2 bounds

---
+++ ComplementaryScripts/EnzymeConstrainedModelRecon/Scripts/FVA_O2.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import multiprocessing
+import os
+import sys
+import time
+
+import numpy as np
+import pandas as pd
+from cobra.flux_analysis import flux_variability_analysis
+
+ecpy_path = '../../../ecpy/'
+sys.path.insert(0, os.path.abspath(ecpy_path))
+import pickle
+
+# from curate_kcat import *
+
+# from multiprocessing import Process,cpu_count,Manager
+
+
+# In[4]:
+# %%
+
+from curate_kcat_light_goslim_asc_step2 import *
+
+results02 = pickle.load(open('../Results/smc_abc_light_step_02.pkl', 'rb'))
+dfpheno = pd.read_csv('../proteomics/phynotype.csv', index_col=0, comment='#')
+dffrac_pro = pd.read_csv('../Results/protein_abundance_go_slim_level_uniq_asc.csv', index_col=0)
+dffrac_mRNA = pd.read_excel('../proteomics/mRNA_abundance_go_slim-20201124.xlsx', sheet_name='mRNA', index_col=0,
+                            header=0)
+mass_columns = [i for i in dffrac_mRNA.columns if 'Mass' in i]
+dffrac_mRNA[mass_columns] = dffrac_mRNA[mass_columns] * 1.651567
+
+dftot = pd.read_csv('../proteomics/total_protein_abandance_mean.csv', index_col=0)
+
+# %%
+
+model_go_pools_file = '../Results/template_ecModel_goslim_pools.pkl'
+model_go_pools = pickle.load(open(model_go_pools_file, 'rb'))
+model_go_pools.objective = 'Biomass_v1'
+model_go_pools.objective_direction = 'max'
+
+# %%
+test_conds = ['NACL60', 'NACL20', 'NACL100', 'HN', 'Fermentation-9h', 'Fermentation-19h', 'Fermentation-30h']
+for cond_id in dftot.index:
+    if cond_id not in dfpheno.index:
+        dfpheno.loc[cond_id, 'P3HB'] = 0
+print()
+
+test_reactions = ['Exchange_AMMONIA', 'Exchange_AMMONIA_REV',
+                  'Exchange_UREA', 'Exchange_UREA_REV',
+                  'Exchange_OXYGEN-MOLECULE', 'Exchange_OXYGEN-MOLECULE_REV',
+                  'Exchange_Glucopyranose'
+                  ]
+
+test_o2 = np.concatenate((np.arange(0, 10, 0.5), np.arange(10, 210, 10)))
+
+
+# %%
+
+def fva_special(model_go_pools, results02, test_conds, dftot, dffrac, output_file):
+    result_df = pd.DataFrame(
+        columns=['RXN_id', 'O2_bound', 'kcat_index', 'condition_id', 'growth', 'minimum', 'maximum'])
+    # result_df_growth = pd.DataFrame(columns=['growth'])
+    test_o2 = np.concatenate((np.arange(0, 10, 0.5), np.arange(10, 210, 10)))
+    for o2_bound in test_o2:
+        kcat_number = -1
+        for kcat in results02.population[:2]:
+            # eModel = pickle.load(open(eModel_file,'rb'))
+            model = model_go_pools.copy()
+            kcat_number += 1
+
+            updateKcats(model, kcat)
+
+            with model:
+
+                for condition_id in test_conds[:1]:
+
+                    print('\nkcat index:', kcat_number, '\nCondition:', condition_id, )
+                    Ptot = dftot.loc[condition_id, 'Ptot']
+                    one_pool, go_pools = buildPoolDict(dffrac, condition_id, Ptot)
+                    pools = go_pools
+                    updateProteinConstraints(model, pools)
+
+                    PHA = dfpheno.loc[condition_id, 'P3HB']
+                    if ~np.isnan(PHA):
+                        model.reactions.PHA_secretion.lower_bound = PHA
+
+                    model.reactions.get_by_id('Exchange_OXYGEN-MOLECULE').bounds = (o2_bound*0.99,o2_bound*1.01)
+
+                    try:
+                        s = model.optimize()
+                        r1 = s.objective_value if s.status == 'optimal' else -1
+
+                        # model.reactions.PHA_secretion.lower_bound = 0
+                        # model.reactions.NGAM.lower_bound = 0
+                        #
+                        # s = model.optimize()
+                        # r2 = s.objective_value if s.status == 'optimal' else -1
+
+                    except:
+                        r1 = -1
+                    print('with pha ngam: %f \n' % (r1))
+                    # df = pd.DataFrame(data={'growth': [r1]}, index=[str(kcat_number) + '_' + condition_id])
+                    # result_df_growth = result_df_growth.append(df)
+
+                    for rea_i in model.reactions:
+                        if rea_i.upper_bound > 9999:
+                            rea_i.upper_bound = 9999
+                    # fva_realist = [i for i in model.reactions if 'prot_' not in i.id]
+                    try:
+                        if r1 != -1:
+                            result_df_i_pro = flux_variability_analysis(model, test_reactions,
+                                                                        fraction_of_optimum=0.95)
+                            # result_df_i_pro.columns = str(kcat_number) + '_' + condition_id + '_' + result_df_i_pro.columns
+                            # result_df = result_df.merge(result_df_i_pro, how='outer', left_index=True, right_index=True, )
+                            result_df_i_pro['RXN_id'] = result_df_i_pro.index
+                            result_df_i_pro['O2_bound'] = o2_bound
+                            result_df_i_pro['kcat_index'] = kcat_number
+                            result_df_i_pro['condition_id'] = condition_id
+                            result_df_i_pro['growth'] = r1
+                            result_df = result_df.append(result_df_i_pro)
+                            # result_df = result_df.reset_index()
+                            # print(result_df)
+                    except:
+                        continue
+                result_df.to_csv(output_file, sep='\t', index=False)
+                # result_df_growth.to_csv(output_file.replace('.tsv', '_growth.tsv'), sep='\t')
+
+
+# %%
+start = time.time()
+
+p1 = multiprocessing.Process(target=fva_special, args=(
+    model_go_pools, results02, test_conds, dftot, dffrac_pro, '../Results/FVA_result_df_pro_1228.tsv'))
+
+p2 = multiprocessing.Process(target=fva_special, args=(
+    model_go_pools, results02, test_conds, dftot, dffrac_mRNA, '../Results/FVA_result_df_mRNA_1228.tsv'))
+p1.start()
+p2.start()
+
+p1.join()
+p2.join()
+
+# fva_special(model_go_pools, results02, test_conds, dftot, dffrac=dffrac_pro,
+#             output_file='../Results/FVA_result_df_pro_1204.tsv')
+#
+#
+# fva_special(model_go_pools, results02, test_conds, dftot, dffrac=dffrac_mRNA,
+#             output_file='../Results/FVA_result_df_mRNA_1204.tsv')
+
+
+print("--- %s seconds ---" % (time.time() - start))
